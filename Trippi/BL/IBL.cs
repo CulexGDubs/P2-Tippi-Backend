b5aa6d197ce6dfffe3b28e7401c047b35bf78572
Nick Yelle <yellencicholas@gmail.com>
2021-10-19 16:58:26 -0400

    AddUser Method

--- Trippi/BL/IBL.cs
+++ Trippi/BL/IBL.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace TrippiBL
     {
     public interface IBL
     {
+        Task<User> AddUserAsync(User user);
     }
 }
--- Trippi/DL/IRepo.cs
+++ Trippi/DL/IRepo.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace DL
     {
     public interface IRepo
         {
+            Task<User> AddUserAsync(User user);
         }
     }
