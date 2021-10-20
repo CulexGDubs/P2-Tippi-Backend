a6538aed699b115fae8d9e95fd416bbf103392c6
Nick Yelle <yellencicholas@gmail.com>
2021-10-20 17:36:51 -0400

    Merge Conflict Resolution & mig

--- Trippi/DL/DBContext.cs
+++ Trippi/DL/DBContext.cs
@@ -7,20 +7,15 @@ using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace DL
-    {
+{
     public class DBContext : DbContext
-        {
-
-
+    {
         public DBContext() : base() { }
-
         public DBContext(DbContextOptions<DBContext> options) : base(options) { }
-
         public DbSet<Friends> Friends { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Rating> Ratings { get; set; }
         public DbSet<Trip> Trips { get; set; }
         public DbSet<UserTripHistory> UserTripHistories { get; set; }
-
-        }
     }
+}
---
+++ Trippi/DL/Migrations/20211020211339_Second.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DL.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trips_Users_UserId",
+                table: "Trips");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Trips",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trips_Users_UserId",
+                table: "Trips",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trips_Users_UserId",
+                table: "Trips");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Trips",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trips_Users_UserId",
+                table: "Trips",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
